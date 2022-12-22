1bc64764c2fb34ad47c4aa906025d579fc81a1ad
Aaron Mark Mina Rios <119110307+bruceblake1@users.noreply.github.com>
2022-12-21 23:00:10 -0500

    Add files via upload

---
+++ EJEMPLO1.py
@@ -0,0 +1,12 @@
+#OPERADORES DE ASIGNACIÓN
+
+a = 0
+a += 7 #suma en asignacion
+a -= 4 #resta en asignacion
+a *= 8 #multiplicacion en asignacion
+a /= 2 #division en asignacion
+a **= 2 #potencia en asignacion
+a /= 128 #division en asignacion
+a -= 8 #resta en asignacion
+
+print(a)
